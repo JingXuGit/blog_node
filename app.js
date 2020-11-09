92bb037f4792e9e20451ade46ddd9ac9a13f99e3
jingxuGit <18310282832@163.com>
2020-11-09 12:13:49 +0800

    配置history路由

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 //使用express构建web服务器 --11:25
 const express = require('express');
-
+const http = require('http')
+const fs = require('fs')
+const httpPort = 8101
 var app = express();
 
 // 注册解析 表单数据的 body-parser
 const bodyParser = require('body-parser')
-    // 将请求响应设置content-type设置为application/json
+// 将请求响应设置content-type设置为application/json
 const router = require('./routes/users.js')
 
-app.use('/api/*', function(req, res, next) {
+app.use('/api/*', function (req, res, next) {
     // 设置请求头为允许跨域
     res.header("Access-Control-Allow-Origin", "*");
     // 设置服务器支持的所有头信息字段
@@ -25,19 +27,19 @@ app.use('/api/*', function(req, res, next) {
 
 // post
 app.use(bodyParser.urlencoded({
-        limit: '50mb',
-        extended: true
-    }))
-    // 处理json格式的参数
+    limit: '50mb',
+    extended: true
+}))
+// 处理json格式的参数
 app.use(bodyParser.json({ limit: '50mb' }))
-    // 配置路由
+// 配置路由
 app.use(router)
 
 
 
-app.listen('8101', function() {
-    console.log('running...')
-})
+// app.listen('8101', function () {
+//     console.log('running...')
+// })
 
 // 端口号
 // var server = app.listen(9527, function () {
@@ -47,3 +49,9 @@ app.listen('8101', function() {
 // //托管静态资源到public目录下
 app.use(express.static('./'));
 app.use(express.static('dist'));
+
+const server = http.createServer(app);
+server.listen(8101, () => {
+    console.log('Listening on %j', server.address());
+});
+
